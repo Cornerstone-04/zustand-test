da6221368168851261269512daa29ee5995824b3
Cornerstone-04 <fortunecornerstone@gmail.com>
2024-01-27 20:48:35 +0100

    feat: change input to textarea

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -14,14 +14,12 @@ export default function Modal({ action, typing, text, close, errorMessage }) {
   }, [errorMessage]);
 
   return (
-    <div className="fixed bg-[rgba(0,0,0,0.3)] w-full h-full top-0 left-0">
+    <div className="fixed bg-[rgba(0,0,0,0.3)] w-full h-full top-0 left-0 backdrop-blur">
       <form
         onSubmit={action}
-        className="bg-white absolute z-10 p-4 top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 h-[6rem] w-[20rem] flex justify-center items-center rounded gap-2"
+        className="bg-white absolute z-10 p-4 top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 min-h-[6rem] w-[20rem] flex flex-col justify-center items-start rounded gap-2"
       >
-        <input
-          type="text"
-          onChange={typing}
+        <textarea
           name="todo"
           value={text}
           className={
@@ -29,12 +27,13 @@ export default function Modal({ action, typing, text, close, errorMessage }) {
               ? "py-1 px-3 border-2 border-red-500 placeholder:text-red-500 placeholder:font-bold transition-all ease-linear"
               : "py-1 px-3 border-2 border-black text-black transition-all ease-linear"
           }
+          onChange={typing}
           placeholder={placeholder}
-        />
-        <button
-          //   onClick={action}
-          className="bg-gray-light text-black py-1 px-3 font-medium"
-        >
+          id=""
+          cols="30"
+          rows="5"
+        ></textarea>
+        <button className="bg-green-500 text-white text-sm font-bold py-1 px-3">
           Submit
         </button>
         <FaTimes
